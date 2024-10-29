894503b3ad964f579c303615a2386481b620c178
Marty Thane <mzf@tuta.io>
2024-10-29 15:09:04 +0100

    first draft of the training script

---
+++ train.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.io import wavfile
+from tensorflow.keras import models, layers
+
+# Tweaking parameters
+EPOCHS = 50
+BATCH_SIZE = 32
+
+# Load the training data
+_, X_train = wavfile.read("in.wav")
+_, y_train = wavfile.read("out.wav")
+
+# Reshape for LSTM layer
+X_train = X_train.reshape(-1,1,1)
+y_train = y_train.reshape(-1,1)
+
+# Define the model architecture
+model = models.Sequential([
+    layers.LSTM(20, input_shape=(1, 1)),
+    layers.Dense(1),
+])
+
+# Compile the model (mazbe change optimizer?)
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Train on data
+model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE)
+
+# Save model to file
+# model.save('model.h5')
